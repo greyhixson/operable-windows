899630cb0a1489968fecdae65de8f240500c3710
Grey Hixson <greyhixson@gmail.com>
2022-01-16 21:13:46 -0600

    Basic functionality

--- src/components/UserWindow.vue
+++ src/components/UserWindow.vue
@@ -1,13 +1,15 @@
 <template>
   <v-container>
     <p>{{ location.city }}, {{ location.state }}</p>
-    <p>{{ weather }} </p>
+    <p>{{ openWindow }}</p>
   </v-container>
 </template>
     
 
 
 <script>
+import { getThresholds } from '../firebase/firebaseinit.js';
+
 export default {
   name: 'UserWindow',
   // *----------------------- P r o p s ----------------------------------------------------------
@@ -18,7 +20,9 @@ export default {
   data() {
     return {
       APIkey: 'fb3f8c4acaba36f086776e594b64a68c',
-      weather: null,
+      weather: Object,
+      windowThresholds: Object,
+      openWindow: null,
     };
   },
   // *----------------------- C o m p u t e d ---------------------------------------------------
@@ -30,13 +34,27 @@ export default {
   // *----------------------- M e t h o d s -----------------------------------------------------
   methods: {
     getCurrentWeather() {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.location.city}&appid=${this.APIkey}`)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.location.city}&appid=${this.APIkey}&units=imperial`)
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          this.weather = JSON.stringify(data);
+          this.weather = data;
+          this.getWindowThresholds()
         });
+    },
+    async getWindowThresholds() {
+      this.windowThresholds = await getThresholds(this.location.city, this.location.state);
+      if (
+        this.windowThresholds.max_temp > this.weather.main.temp &&
+        this.windowThresholds.min_temp < this.weather.main.temp &&
+        this.windowThresholds.humidity_max > this.weather.humidity
+      ) {
+        this.openWindow = 'Open Window';
+      }
+      else {
+        this.openWindow = 'Close Window';
+      }
     },
   },
   // *----------------------- W a t c h ---------------------------------------------------------
--- src/firebase/firebaseinit.js
+++ src/firebase/firebaseinit.js
@@ -1,14 +1,28 @@
 import { initializeApp } from "firebase/app";
-import "firebase/firestore"
+import { 
+  getFirestore, collection, getDocs
+} from "firebase/firestore"
 
-const firebaseConfig = {
+const firebaseApp = initializeApp({
     apiKey: "AIzaSyCBg6Qdq-gOWa3yA_9zP62mzXRv8yDi7sI",
     authDomain: "operable-windows.firebaseapp.com",
     projectId: "operable-windows",
     storageBucket: "operable-windows.appspot.com",
     messagingSenderId: "379289738007",
     appId: "1:379289738007:web:343a97bc2900ebb8303552"
-  };
+  });
   
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export async function getThresholds(city, state) {
+  const querySnapshot = await getDocs(collection(db, "window-thresholds"));
+  var data;
+  querySnapshot.forEach((doc) => {
+    if(doc.data().city == city && doc.data().state == state){
+      data = doc.data();
+    }
+  });
+  return data;
+}
+
+
